1e10e265f632d41caba29488aa83df3c6a511b28
Amiran Kurman <amiranfox@gmail.com>
2021-12-05 00:32:25 +0600

    yeah boi

--- Assets/Scripts/Mech/Door.cs
+++ Assets/Scripts/Mech/Door.cs
@@ -2,37 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour, Mechanism
+public class Door : BaseMech
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Activate()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void DeActivate()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public bool IsActivated()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public bool IsInteractable()
-    {
-        throw new System.NotImplementedException();
-    }
+    
 }
